83ebac24376c89ce24f8b42e7ea8d93fbaa34fc1
Roman Zbruev <roman.zbruev@yandex.ru>
2022-09-06 17:15:12 +0300

    refactor: Сделан обработчик ошибок апи-токена + локализация сообщений об ошибке

--- src/main/java/app/filatov/homeworkstatusesbot/bot/handle/texthandler/message/ErrorHandler.java
+++ src/main/java/app/filatov/homeworkstatusesbot/bot/handle/texthandler/message/ErrorHandler.java
@@ -3,10 +3,14 @@ package app.filatov.homeworkstatusesbot.bot.handle.texthandler.message;
 import app.filatov.homeworkstatusesbot.bot.handle.language.LanguageSupplier;
 import app.filatov.homeworkstatusesbot.bot.handle.texthandler.state.UserState;
 import app.filatov.homeworkstatusesbot.bot.handle.texthandler.util.HandlerUtil;
+import app.filatov.homeworkstatusesbot.exception.UserNotFoundException;
+import app.filatov.homeworkstatusesbot.model.User;
 import app.filatov.homeworkstatusesbot.model.repository.UserRepository;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
+
+import java.util.Optional;
 
 @Component
 public class ErrorHandler implements MessageHandler {
@@ -30,8 +34,14 @@ public class ErrorHandler implements MessageHandler {
     @Override
     public SendMessage handle(Message message) {
         Long chatId = message.getChatId();
-        userRepository.findById(message.getFrom().getId()).ifPresent(user -> user.setApiKey(null));
-        userRepository.findById(message.getFrom().getId()).ifPresent(util::setCorrectStateForUser);
+        Optional<User> optional = userRepository.findById(message.getFrom().getId());
+        if (optional.isEmpty()) {
+            throw new UserNotFoundException("Пользователь не найден");
+        }
+        User user = optional.get();
+        user.setApiKey(null);
+        util.setCorrectStateForUser(user);
+        userRepository.save(user);
         return new SendMessage(String.valueOf(chatId),
                 messageService.getMessage("message.error", languageSupplier.getLanguage(message)));
     }
